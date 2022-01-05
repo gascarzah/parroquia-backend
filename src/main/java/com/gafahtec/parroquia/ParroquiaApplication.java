6e329c1cf39b491251abf68f40ea71780540220f
unknown <gascarzah@gmail.com>
2022-01-04 19:02:14 -0500

    first commit

---
+++ src/main/java/com/gafahtec/parroquia/ParroquiaApplication.java
@@ -0,0 +1,13 @@
+package com.gafahtec.parroquia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParroquiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParroquiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gafahtec/parroquia/ParroquiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gafahtec.parroquia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParroquiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
